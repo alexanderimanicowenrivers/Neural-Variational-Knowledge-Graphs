48a71632ffa7d7e2c18c75f18567830655bfc8df
acr42 <mc_rivers@icloud.com>
2018-06-14 12:01:11 +0100

    added models into init

--- vkge/__init__.py
+++ vkge/__init__.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from vkge.base import VKGE
+from vkge.base2 import VKGE2
+from vkge.base3 import VKGE_simple
 
-__all__ = ['VKGE']
+__all__ = ['VKGE','VKGE2','VKGE_simple']
