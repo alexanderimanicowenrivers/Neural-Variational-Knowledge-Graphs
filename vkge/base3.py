89ec8731f18f20d076142a13b4ef9ea8a5f6f5e9
acr42 <mc_rivers@icloud.com>
2018-06-19 15:56:58 +0100

    updated decaying KL

--- vkge/base3.py
+++ vkge/base3.py
@@ -112,8 +112,7 @@ class VKGE2:
         self.nb_entities, self.nb_predicates = len(entity_set), len(predicate_set)
         ############################
 
-        optimizer = tf.train.AdamOptimizer(learning_rate=lr, beta1=b1, beta2=b2, epsilon=eps)
-        # optimizer=tf.train.AdagradOptimizer(learning_rate=0.1)
+        optimizer = tf.train.AdagradOptimizer(learning_rate=0.1)        # optimizer=tf.train.AdagradOptimizer(learning_rate=0.1)
         self.build_model(self.nb_entities, entity_embedding_size, self.nb_predicates, predicate_embedding_size,
                          optimizer,
                          ent_sigma, pred_sigma)
