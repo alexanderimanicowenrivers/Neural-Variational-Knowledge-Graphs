9bdd71e08291d4357eca7289223c60bcf48dd2d9
acr42 <mc_rivers@icloud.com>
2018-06-23 10:55:14 +0100

    updated file names

--- vkge/__init__.py
+++ vkge/__init__.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 
-from vkge.base3 import VKGE2
+from vkge.base_model_search import VKGE2
 from vkge.base import VKGE
-from vkge.base2 import VKGE_simple
+from vkge.non_probabilistic import VKGE_simple
 from vkge.basic_prop import VKGE_working
 
 __all__ = ['VKGE2','VKGE','VKGE_simple','VKGE_working']
