a36c7b76b95063a06f580e7a1116f6acdb459fb6
acr42 <mc_rivers@icloud.com>
2018-06-26 20:26:56 +0100

    added in test phase print statement

--- script_quicktest.py
+++ script_quicktest.py
@@ -136,6 +136,8 @@ export PYTHONPATH=.
 
     for job_id, command_line in enumerate(sorted_command_lines, 1):
         print('test $SGE_TASK_ID -eq {} && {}'.format(job_id, command_line))
+    for job_id2, command_line in enumerate(sorted_command_lines, 1):
+        print('test $SGE_TASK_ID -eq {} && {}'.format(job_id+job_id2, command_line))
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO)
