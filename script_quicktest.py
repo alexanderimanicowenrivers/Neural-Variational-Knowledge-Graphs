3dcd1a3a1ed9ae023aea4a7633fbfc14e6b47347
acr42 <mc_rivers@icloud.com>
2018-06-26 11:51:36 +0100

    added in MLE

--- script_quicktest.py
+++ script_quicktest.py
@@ -65,9 +65,9 @@ def to_logfile(c, path):
 
 def main(_):
     hyperparameters_space = dict(
-        w1=[5],
-        w2=[0.5], #
-        w3=[200],
+        w1=[2],
+        w2=[0.02], #
+        w3=[300],
         w4=[True],
         w5=[True],
         w6 = [False],
