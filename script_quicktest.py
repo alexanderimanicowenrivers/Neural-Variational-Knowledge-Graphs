604256b33705e3cb07f6915b0abf9bfcd43ac050
acr42 <mc_rivers@icloud.com>
2018-06-26 22:34:37 +0100

    added in test phase print statement

--- script_quicktest.py
+++ script_quicktest.py
@@ -52,7 +52,7 @@ def to_cmd(c):
                 c['w6'],
                 c['w7'],
                 c['w8'],
-                "{}/logs/18_6_25/tb_nvkg.{}".format(path, summary(c)),
+                "{}/logs/18_6_25_hinge/tb_nvkg.{}".format(path, summary(c)),
                 c['w9'],
                 )
     return command
@@ -79,7 +79,7 @@ def main(_):
 
     configurations = cartesian_product(hyperparameters_space)
 
-    path = '/home/acowenri/workspace/Neural-Variational-Knowledge-Graphs/logs/18_6_25'
+    path = '/home/acowenri/workspace/Neural-Variational-Knowledge-Graphs/logs/18_6_25_hinge'
 
     # Check that we are on the UCLCS cluster first
     if os.path.exists('/home/acowenri/'):
