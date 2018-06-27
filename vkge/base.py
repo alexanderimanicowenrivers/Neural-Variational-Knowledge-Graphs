47b69bdb88336741e3f788acdfd975f3d0973980
acr42 <mc_rivers@icloud.com>
2018-06-27 10:33:01 +0100

    put hinge back

--- vkge/base.py
+++ vkge/base.py
@@ -434,6 +434,12 @@ class VKGE:
         with tf.Session() as session:
             session.run(init_op)
 
+            logger.warn('PRINTING TOP 20 ROWS FROM SAMPLE ENTITY MEAN AND VAR ')
+
+            samp1_mu, samp1_sig = session.run(self.var1_1, self.var1_2)
+
+            logger.warn('Sample Mean \t {} \t Sample Var \t {}'.format(samp1_mu[:20], samp1_sig[:20]))
+
             train_writer = tf.summary.FileWriter(filename, session.graph)
 
             for epoch in range(1, nb_epochs + 1):
