256bfde80e0613a8235e0197917a5e88e67cd6a9
acr42 <mc_rivers@icloud.com>
2018-07-05 16:02:29 +0100

    try a single kl

--- script_quicktest.py
+++ script_quicktest.py
@@ -48,7 +48,7 @@ def to_cmd(c):
                 c['w6'],
                 c['w7'],
                 c['w8'],
-                "{}/logs/18_7_05_nokl/tb_nvkg.{}".format(path, summary(c)),
+                "{}/logs/18_7_05_onekl/tb_nvkg.{}".format(path, summary(c)),
                 c['w9']
                 )
     return command
@@ -72,7 +72,7 @@ def main(_):
 
     configurations = cartesian_product(hyperparameters_space)
 
-    path = '/home/acowenri/workspace/Neural-Variational-Knowledge-Graphs/logs/18_7_05_nokl'
+    path = '/home/acowenri/workspace/Neural-Variational-Knowledge-Graphs/logs/18_7_05_onekl'
 
     # Check that we are on the UCLCS cluster first
     if os.path.exists('/home/acowenri/'):
