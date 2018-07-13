17cbec96cf5e404bd9057df8eb82f9dff24c70e1
acr42 <mc_rivers@icloud.com>
2018-07-13 09:15:02 +0100

    upload GenieAI

--- vkge/__init__.py
+++ vkge/__init__.py
@@ -4,5 +4,6 @@ from vkge.base_model_search import VKGE2
 from vkge.base import VKGE
 from vkge.non_probabilistic import VKGE_simple
 from vkge.basic_prop import VKGE_working
+import vkge.GinieAI import GinieAI
 
 __all__ = ['VKGE2','VKGE','VKGE_simple','VKGE_working','GenieAI']
