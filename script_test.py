4fb10b5e5e5358254b8a541456e43c7bee49b68b
acr42 <mc_rivers@icloud.com>
2018-07-24 17:35:35 +0100

    added in full matrix of LCWA test

--- script_test.py
+++ script_test.py
@@ -63,7 +63,7 @@ def to_logfile(c, path):
 
 def main(_):
     hyperparameters_space = dict(
-        w1=[1,10,100],
+        w1=[1,100,1000],
         w2=[1e-3,1e-5,1e-7], #
         w3=[10,20,50,100,200],
         w6 = ['kinship','nations','umls'],
