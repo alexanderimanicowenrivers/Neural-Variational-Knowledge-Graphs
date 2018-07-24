548a6bdd73a0afb733f7da5f2f0b0af7afced57e
acr42 <mc_rivers@icloud.com>
2018-07-24 17:33:27 +0100

    added in full matrix of LCWA test

--- script_test.py
+++ script_test.py
@@ -38,8 +38,8 @@ def to_cmd(c):
               '--alt_updates {} ' \
               '--lr {} ' \
               '--mean_c {} ' \
-              '--score_func {}' \
-              '--alt_opt {}' \
+              '--score_func {} ' \
+              '--alt_opt {} ' \
         .format(path,
                 #                 params,
                 #                 set_to_path[c['instances']],
