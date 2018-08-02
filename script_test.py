78dbbc17521ddd7327f40424d26159b0b0525cc5
acr42 <mc_rivers@icloud.com>
2018-08-02 16:18:20 +0100

    upload experiment 2 of 5 negative samples

--- script_test.py
+++ script_test.py
@@ -66,7 +66,8 @@ def to_logfile(c, path):
 
 def main(_):
     hyperparameters_space = dict(
-        w1=[10000,1000],
+        # w1=[10000,1000],
+        w1=[10],
         w2=[1e-3,1e-5,1e-7], #
         w3=[10,50,100,200],
         w4 = ['kinship','nations','umls'],
