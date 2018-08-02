6a75709b78e932030bcdbdbec136f006a1d03a5b
acr42 <mc_rivers@icloud.com>
2018-08-02 21:15:26 +0100

    update evaluation

--- script_test.py
+++ script_test.py
@@ -66,7 +66,7 @@ def to_logfile(c, path):
 
 def main(_):
     hyperparameters_space = dict(
-        w1=[1000,100],
+        w1=[10000,100],
         # w1=[10],
         w2=[1e-3,1e-5,1e-7], #
         w3=[50,100,200,300],
