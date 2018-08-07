11163c74c3007fdabf2e16a783a65d6c78f931eb
acr42 <mc_rivers@icloud.com>
2018-08-07 11:27:54 +0100

    updated folder name

--- vkge/__init__.py
+++ vkge/__init__.py
@@ -1,10 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from vkge.base_model_search import VKGE2
 from vkge.base import VKGE
 from vkge.baseline import VKGE_simple
-from vkge.basic_prop import VKGE_working
-from vkge.base_variance_tests import VKGE_justified
-from vkge.base_quick import VKGE_quick
 
-__all__ = ['VKGE_justified','VKGE2','VKGE','VKGE_simple','VKGE_working','VKGE_quick']
+__all__ = ['VKGE','VKGE_simple']
