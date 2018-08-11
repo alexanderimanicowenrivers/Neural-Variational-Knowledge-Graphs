4ed3df22518b5310bb9a6498b178569b08244e0f
acr42 <mc_rivers@icloud.com>
2018-08-11 13:02:05 +0100

    updated lr for model1 large datasets

--- scripts/script_vkge.py
+++ scripts/script_vkge.py
@@ -75,7 +75,7 @@ def main(_):
         w3=[200],
         w4 = ['fb15k-237', 'wn18', 'wn18rr'],
         w5=[False],
-        w6=[0.01,0.001],
+        w6=[0.001],
         w7=['DistMult','TransE', 'ComplEx'],
         w8=[100,1000],
         w9=['True'])
