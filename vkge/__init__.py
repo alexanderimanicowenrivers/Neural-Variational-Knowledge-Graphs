0b9df22a222ed99132f4bd3b8993a04ae4435c08
acr42 <mc_rivers@icloud.com>
2018-08-12 22:43:09 +0100

    compression cost

--- vkge/__init__.py
+++ vkge/__init__.py
@@ -5,6 +5,6 @@ from vkge.baseline import VKGE_simple
 from vkge.base_tests import VKGE_tests
 from vkge.base_model2 import VKGE_A
 # from vkge.copy_working import VKGE_working
-from vkge.base_quick import VKGE_quick
-__all__ = ['VKGE','VKGE_simple','VKGE_tests','VKGE_working','VKGE_quick']
-# __all__ = ['VKGE','VKGE_simple','VKGE_tests','VKGE_A']
+# from vkge.base_quick import VKGE_quick
+# __all__ = ['VKGE','VKGE_simple','VKGE_tests','VKGE_working','VKGE_quick']
+__all__ = ['VKGE','VKGE_simple','VKGE_tests','VKGE_A']
