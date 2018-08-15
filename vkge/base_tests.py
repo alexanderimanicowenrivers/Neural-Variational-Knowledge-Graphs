4b7ca3f22e0f544f5794c76dc781c53c28457318
acr42 <mc_rivers@icloud.com>
2018-08-15 19:14:19 +0100

    test3 var

--- vkge/base_tests.py
+++ vkge/base_tests.py
@@ -111,7 +111,7 @@ class VKGE_tests:
         self.alt_opt=alt_opt
         self.nosamps=int(nosamps)
 
-        self.no_confidence_samples=2
+        self.no_confidence_samples=300
 
                 # adjust for correct format for model input
 
@@ -747,7 +747,7 @@ class VKGE_tests:
                 # logger.warn('Epoch: {0}\t Negative ELBO: {1}'.format(epoch, self.stats(loss_values)))
 
             cvrg=[]
-            for p_threshold in np.arange(0,1,0.5):
+            for p_threshold in np.arange(0,1,0.01):
                 cvrg.append(1-p_threshold)
                 # self._saver.save(session, filename+'_epoch_'+str(epoch)+'.ckpt')
 
