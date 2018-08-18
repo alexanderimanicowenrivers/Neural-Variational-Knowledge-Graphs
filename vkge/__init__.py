5cd5017d5c4b5a461981b9a1f064848053345024
acr42 <mc_rivers@icloud.com>
2018-08-18 17:16:28 +0100

    modified compression cost KL model A

--- vkge/__init__.py
+++ vkge/__init__.py
@@ -2,9 +2,9 @@
 
 from vkge.base import VKGE
 from vkge.baseline import VKGE_simple
-from vkge.base_tests import VKGE_tests
+# from vkge.base_tests import VKGE_tests
 from vkge.base_model2 import VKGE_A
-# from vkge.copy_working import VKGE_working
-# from vkge.base_quick import VKGE_quick
-# __all__ = ['VKGE','VKGE_simple','VKGE_tests','VKGE_working','VKGE_quick']
-__all__ = ['VKGE','VKGE_simple','VKGE_tests','VKGE_A']
+from vkge.copy_working import VKGE_working
+from vkge.base_quick import VKGE_quick
+__all__ = ['VKGE','VKGE_simple','VKGE_working','VKGE_quick']
+# __all__ = ['VKGE','VKGE_simple','VKGE_tests','VKGE_A']
