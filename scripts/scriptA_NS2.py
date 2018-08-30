3d873c4010d10eeb6610be071b81ce5a492c56fc
acr42 <mc_rivers@icloud.com>
2018-08-31 00:07:49 +0100

    rerun-complex

--- scripts/scriptA_NS2.py
+++ scripts/scriptA_NS2.py
@@ -75,11 +75,11 @@ def main(_):
         w3=[300],
         # w4=['kinship', 'nations', 'umls'],
         w4 = ['fb15k-237', 'wn18', 'wn18rr'],
-        w5=[False,True],
+        w5=[False],
         w6=[1e-3],
         w7=['ComplEx'],
         w8=[0,1,2,3,4,5,6],
-        w9=[False,True])
+        w9=[False])
 
     configurations = cartesian_product(hyperparameters_space)
     path = '/home/acowenri/workspace/Neural-Variational-Knowledge-Graphs/logs/modela_noprojecion'
