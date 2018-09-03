16a8ea6b1d3ba7239cce0ca19daceb97c4b63e94
acr42 <mc_rivers@icloud.com>
2018-09-03 17:10:52 +0100

    model B NS

--- scripts/scriptB.py
+++ scripts/scriptB.py
@@ -57,7 +57,7 @@ def to_cmd(c):
                 c['w7'],
                 c['w8'],
                 c['w9'],
-                ("{}/logs/ModelB_NS/uclcs_nvkg_v1.{}".format(path, summary(c)))
+                ("{}/logs/ModelB_BS/uclcs_nvkg_v1.{}".format(path, summary(c)))
                 )
     return command
 
@@ -81,7 +81,7 @@ def main(_):
         w9=[True,False])
 
     configurations = cartesian_product(hyperparameters_space)
-    path = '/home/acowenri/workspace/Neural-Variational-Knowledge-Graphs/logs/ModelB_NS'
+    path = '/home/acowenri/workspace/Neural-Variational-Knowledge-Graphs/logs/ModelB_BS'
 
 
     # Check that we are on the UCLCS cluster first
