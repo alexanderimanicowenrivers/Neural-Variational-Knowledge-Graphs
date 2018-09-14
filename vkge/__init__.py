28e7c2c6cec19aa2cb02a44bd186c64691f2dde1
acr42 <mc_rivers@icloud.com>
2018-09-14 14:56:56 +0100

    testing util package

--- vkge/__init__.py
+++ vkge/__init__.py
@@ -6,5 +6,5 @@ from vkge.base_tests import VKGE_tests
 from vkge.base_modela import VKGE_A
 from vkge.baseA import modelA
 from vkge.baseB import modelB
-from vkge.util import  make_batches,read_triples,IndexGenerator
+from vkge.training.util import  make_batches,read_triples,IndexGenerator
 __all__ = ['VKGE','VKGE_simple','VKGE_tests','VKGE_A','modelB','modelA',"IndexGenerator"]
