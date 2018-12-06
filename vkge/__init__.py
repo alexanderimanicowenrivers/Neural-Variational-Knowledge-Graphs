09efbbf5092c841fac67e3d1ec4bd3c0968159cd
alexanderimanicowen-rives <mc_rivers@icloud.con>
2018-12-06 12:15:47 +0000

    fixed import of LIM

--- vkge/__init__.py
+++ vkge/__init__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from vkge.LCM import LIM
+from vkge.LIM import LIM
 from vkge.LFM import LFM
 from vkge.training.util import  make_batches,read_triples,IndexGenerator
 __all__ = ['LFM','LIM']
